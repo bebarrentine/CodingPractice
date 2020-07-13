04a0e0535bb26cab6ae8ef55c39f0abc78640ada
Bri Barrentine <briannabarrentine@gmail.com>
2020-07-13 13:34:02 -0500

    Posting LeapYear exercise

---
+++ LeapYear.py
@@ -0,0 +1,26 @@
+# Developer:
+#
+# Date Started:
+#
+# Date Completed:
+#
+# File name: LeapYear.py
+#
+# Program Description: The following program is used to display all
+# of the leap years in the range entered by the user
+
+# Ask the user for a starting year and ending year in order to
+# calculate the amount of leap years between the two years
+startingYear = #?
+endingYear = #?
+
+# Use a for loop, if/elif statements, and print statements to
+# determine and then display which years between the start and
+# end years are leap years
+for
+    # Follow the logic: if the year is divisible by 4 it is a leap year
+    if
+        print
+    # Follow the logic: if the year is divisible by 4, 100, and 400 it is a leap year
+    elif
+        print
